9383b8c21c9e69c795311bf4e51fff1fa79e9493
jyothi <thotajyothi19@gmail.com>
2024-08-22 14:58:44 +0530

    main.tfupdates

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -95,8 +95,8 @@ resource "aws_eks_cluster" "eks" {
 
   vpc_config {
     subnet_ids = [
-      aws_subnet.public-subnet-01.id,
-      aws_subnet.public-subnet-02.id
+      data.aws_subnet.public-subnet-01.id,
+      data.aws_subnet.public-subnet-02.id
     ]
   }
 
@@ -112,8 +112,8 @@ resource "aws_eks_node_group" "node-grp" {
   node_group_name = "dev-node-group"
   node_role_arn   = aws_iam_role.worker.arn
   subnet_ids      = [
-    aws_subnet.public-subnet-01.id,
-    aws_subnet.public-subnet-02.id
+    data.aws_subnet.public-subnet-01.id,
+    data.aws_subnet.public-subnet-02.id
   ]
   instance_types  = ["t2.small"]
   disk_size       = 20
@@ -135,5 +135,6 @@ resource "aws_eks_node_group" "node-grp" {
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
   ]
 }
+
 
 
