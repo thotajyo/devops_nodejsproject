56427305c82f5d28fc54fb10b8c1e93109edeb54
jyothi <thotajyothi19@gmail.com>
2024-08-22 15:17:22 +0530

    updates2

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -57,6 +57,23 @@ resource "aws_iam_instance_profile" "worker" {
   role       = aws_iam_role.worker.name
 }
 
+
+# Attach necessary policies to the worker node role
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.worker.name
+}
+
 # data source 
  data "aws_vpc" "vpc" {
   tags = {
